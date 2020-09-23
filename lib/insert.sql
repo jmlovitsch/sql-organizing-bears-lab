e9b2525a9c3d8057ba922e1397a39e86eeb58e7b
john <jmlovitsch@gmail.com>
2020-09-22 19:33:02 -0500

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Butter', 'Chill', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'F', 'White', 'Angry', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Tabitha', 13, 'M', 'Black', 'Hot', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 31, 'Brown', 'Red', 'Chill', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Melissa', 21, 'F', 'White', 'Angry', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Grinch', 51, 'M', 'Black', 'Sad', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Red', 'Happy', 1);
-ADD INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL,  10, 'M', 'Red', 'Chill', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 1, 'M', 'Butter', 'Chill', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Rowdy', 4, 'F', 'White', 'Angry', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Tabitha', 13, 'M', 'Black', 'Hot', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Sergeant Brown', 31, 'Brown', 'Red', 'Chill', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Melissa', 21, 'F', 'White', 'Angry', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, 'Grinch', 51, 'M', 'Black', 'Sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, 'Wendy', 11, 'F', 'Red', 'Happy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL,  10, 'M', 'Red', 'Chill', 1);
